3f02afced84aa7203ce5c7fee2eefdccc4041fad
khangduy2008 <77973360+khangduy2008@users.noreply.github.com>
2021-10-14 20:03:16 +0700

    kmkk

--- webintensive/chat/index.js
+++ webintensive/chat/index.js
@@ -41,6 +41,131 @@ let getRealTime = () => {
   return time;
 };
 
+
 setInterval(function () {
   clock.innerHTML = getRealTime();
 }, 1000);
+
+window.onload = init
+
+async function init(){
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user && user.emailVerified) {
+      loadConversations(user.email)
+
+    } else {
+      sweetAlert("warning","you need to login to chat")
+      setTimeout(function(){ open("./signin.html","_self") }, 3000);
+    }
+  });
+}
+
+let loadConversations = async (email) =>{
+  let currenEmail = email.trim()
+  document.querySelector("#currentEmail").innerHTML = currenEmail
+  let result = await firebase.firestore().collection('chat').where('user', 'array-contains', currenEmail).get()
+  
+
+  let data = getDataFromDocs(result.docs)
+  console.log(data);
+  rederChat(data[0],currenEmail)
+  rederListUsers(data, currenEmail)
+};
+
+let rederChat = (data,email) =>{
+  let dom = document.querySelector(".chat_content")
+  let chat_name = document.querySelector("#currentName")
+  let chat_id = document.querySelector("#currentId")
+  chat_name.innerHTML= data.name
+  chat_id.innerHTML=data.id
+  dom.innerHTML=''
+
+  for(let i = 0; i < data.messages.length; i++) {
+    let chatClass='message'
+    if(data.messages[i].owner == email){
+     chatClass = "message owner"   
+    }
+    let html = `<div class="${chatClass}">
+    <div class="message_info">
+      <span>${data.messages[i].owner}</span>
+      <span>${data.messages[i].time}</span>
+    </div>
+    <span class="content_m">${data.messages[i].content}</span>
+  </div>`
+  dom.innerHTML +=html
+  }
+
+      
+      
+}
+let rederListUsers = (data,email)=> {
+  let dom = document.querySelector(".userlist")
+  dom.innerHTML = ""
+  for(let i =0; i<data.length; i++){
+    let html = ` <div id="c${data[i].id}" class="user">
+    <span><img id="ava" src="${data[i].img}">${data[i].name} </span>
+    <span>${data[i].createAt}</span>
+</div>`
+
+dom.innerHTML +=html
+  }
+  for(let i =0; i<data.length; i++){
+    let user = document.querySelector(`#c${data[i].id}`)
+    user.onclick = ()=>{
+      rederChat(data[i],email)
+    }
+  }
+}
+ 
+
+
+ let getDataFromDoc = (doc)=>{
+  let data = doc.data()
+  data.id = doc.id
+  return data
+
+};
+
+let getDataFromDocs = (docs)=>{
+ let result =[]
+ for(let doc of docs){
+     let data = getDataFromDoc(doc)
+     result.push(data)
+  }
+ return result
+};
+
+let signOut = ()=>{
+  firebase.auth().signOut().then(() => {
+    open("./signin.html","_self")
+  }).catch((error) => {
+    sweetAlert("error",error)
+  });
+
+}
+
+let sweetAlert = (icon, content) => {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+
+  Toast.fire({
+    icon: icon,
+    title: content,
+  });
+};
+
+
+let forminputMessage = document.querySelector("#sent_messages")
+forminputMessage.onsubmit = (e)=>{
+  e.pre
+}
+
